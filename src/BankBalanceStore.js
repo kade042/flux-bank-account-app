154449150d8b277451f0ea2ce8b849c2dd7799fe
kade042 <dereje88@gmail.com>
2017-06-20 22:37:04 -0600

    using Container from flux utils

--- src/BankBalanceStore.js
+++ src/BankBalanceStore.js
@@ -1,28 +1,22 @@
-import { Store } from 'flux/utils';
+import { ReduceStore } from 'flux/utils';
 import AppDispatcher from './AppDispatcher';
 import bankConstants from './constants';
 
-let balance = 0;
-
-class BankBalanceStore extends Store {
-  getState() {
-    return balance;
+class BankBalanceStore extends ReduceStore {
+  getInitialState() {
+    return 0;
   }
 
-  __onDispatch(action) {
+  reduce(state, action) {
     switch (action.type) {
       case bankConstants.CREATE_ACCOUNT:
-        balance = 0;
-        this.__emitChange();
-        break;
+        return 0;
       case bankConstants.DEPOSITED_INTO_ACCOUNT:
-        balance = balance + action.amount;
-        this.__emitChange();
-        break;
+        return state + action.amount;
       case bankConstants.WITHDREW_FROM_ACCOUNT:
-        balance = balance - action.amount;
-        this.__emitChange();
-        break;
+        return state - action.amount;
+      default:
+        return state;
     }
   }
 }
